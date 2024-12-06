64a0dfbc3535e71185005c28be49b43a6d177362
Andrew Kimwetich <kimwetichandrew@gmail.com>
2024-12-06 14:44:41 +0300

    integers

---
+++ numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿//int => 32-bit numbers
+
+int a = 1;
+
+
+
+//long => 64-bit (large numbers)
+
+long b = 2000000000000;
+
+Console.WriteLine(a + b);
+
+//decimal => small range but great precision
+
+decimal d = 3.2M;
+
+decimal i = 4.240M;
+
+decimal j = 4.537M;
+
+Console.WriteLine($"decimal: {i+j}");
+
+
+//float && double => wide range less precise
+
+double e = 4.2403834746;
+
+float f = 4.53783346F;
+
+Console.WriteLine($"double & float: {e+f}");
