4c14816bdefaa2569eb246014ff3a94af57faed7
Andrew Kimwetich <kimwetichandrew@gmail.com>
2024-12-08 08:56:51 +0300

    Language intergrated Querry (LINQ)

---
+++ LINQ/Program.cs
@@ -0,0 +1,26 @@
+﻿//LINQ => Language integrated querry
+
+
+List<int> scores = [1,45,23,52,67,99,75,38,10,15]; 
+
+var scores2 = new List<int> {1,45,23,52,67,99,75,38,10,15};
+
+IEnumerable<int> scoresQuerry = 
+    from score in scores
+    where score > 50
+    orderby score descending
+    select score;
+
+foreach(var i in scoresQuerry){
+    Console.WriteLine(i);
+};
+
+
+
+//approach 2: using method syntax 
+
+IEnumerable<int> scoresQuerry2 = scores.Where(s => s > 50).OrderByDescending(s=>s);
+
+foreach(var i in scoresQuerry2){
+    Console.WriteLine(i);
+};
