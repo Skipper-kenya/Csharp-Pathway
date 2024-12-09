35a82627013f5609326aea6daf19c6c251466ba8
Andrew Kimwetich <kimwetichandrew@gmail.com>
2024-12-09 07:59:24 +0300

    test folder

---
+++ test/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MyNamespace {
+    public class myApp {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
